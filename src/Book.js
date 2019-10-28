aed67920f046cb2fdc3b56a5bdb46a9b5cd8d0d7
Kevin Duell <kdubzot9@gmail.com>
2019-10-27 19:31:54 -0700

    Successfully set state from constructor.

--- src/Book.js
+++ src/Book.js
@@ -14,15 +14,19 @@ class Book extends Component {
   {value:"wantToRead",text:"Want to Read"},
   {value:"read",text:"Read"},
   {value:"none",text:"None"}];
-  constructor(bookStateIn) {
-    super();
+  constructor(argumentList) {
+    super(argumentList);
+;
+this.setState({
+      bookState: argumentList.bookState
+    });
+console.log(argumentList.bookState);
 
-
-  this.setState({bookState:bookStateIn});
 
 
   }
   render() {
+
     return(<div className="book">
                          <div className="book-top">
                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
@@ -30,7 +34,7 @@ class Book extends Component {
                              <select>
                               {this.possibleBookStates.map((obj) => <option value={obj.value}>{obj.text}</option>)}
 
-                          
+
                              </select>
                            </div>
                          </div>
