9c71996a8e9b7d394390d86e099b4df1d68132d9
Kevin Duell <kdubzot9@gmail.com>
2019-10-30 08:19:53 -0700

    Capture progress Book, shelf

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-
-
+/*
+  *   <Book bookState="currentlyReading" title="Ender's Game" author="Orson Scott Card" updateView={this.updateView}/>
+  */
 
 class Book extends Component {
   state = {
       bookState: ""
     }
 
-  possibleBookStates =[{value:"move",text:"Move to..."},
+  possibleShelves =[{value:"move",text:"Move to..."},
   {value:"currentlyReading",text:"Currently Reading"},
   {value:"wantToRead",text:"Want to Read"},
   {value:"read",text:"Read"},
   {value:"none",text:"None"}];
   constructor(argumentList) {
     super(argumentList);
-    this.updateViewFromChild = this.updateViewFromChild.bind(this)
+  //  this.updateViewFromChild = this.updateViewFromChild.bind(this)
+
     this.setState({
-      bookState: argumentList.bookState
+      shelf: argumentList.shelf
     });
 
 
 
   }
+updateViewFromChild = (e) => {
+//  updateViewFromChild(e){
+    //  console.log(e.target.value);
+    // set the state for the ui componentDidMount
 
-  updateViewFromChild(e){
-      console.log(e.target.value);
-   this.props.updateView();
+    this.setState()
+//   this.props.updateView();
    }
 
   handleChange(e)  {
@@ -41,8 +46,8 @@ class Book extends Component {
                          <div className="book-top">
                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
                            <div className="book-shelf-changer">
-                             <select onChange={this.updateViewFromChild} value={this.state.bookState}>
-                              {this.possibleBookStates.map((obj) => <option value={obj.value}>{obj.text}</option>)}
+                             <select onChange={this.updateViewFromChild} value={this.state.shelf}>
+                              {this.possibleShelves.map((obj) => <option key={obj.value} value={obj.value}>{obj.text}</option>)}
                               </select>
                            </div>
                          </div>
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
+import Book from './Book.js'
 import PropTypes from 'prop-types'
 
 class Shelf extends Component {
+  state = {
+         booksOnShelf:[]
+  }
+
+  constructor(props)   {
+    console.log(props.allBooks.length);
+  }
+
+
+
+  render() {
+    return( <div className="bookshelf">
+                  <h2 className="bookshelf-title">{this.props.label}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      <li>
+                    <Book bookState="currentlyReading" title="Ender's Game" author="Orson Scott Card" />
+                      </li>
+                      <li>
+
+
+                      </li>
+                    </ol>
+                  </div>
+                </div>)
+  }
 
 }
 
-export default Book
+export default Shelf
